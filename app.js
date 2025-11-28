275587410565a6c8c42c739402a848a421e60d2c
H1nkk <the_h1nk@mail.ru>
2025-11-28 22:58:46 +0300

    add database integration (works with user.html)

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const path = require('path');
 const pool = require('./db_test.js');
-
 const app = express();
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // ?????????
 
 pool.query('SELECT NOW()', function(err, res) {
     if (err) {
@@ -11,7 +15,39 @@ pool.query('SELECT NOW()', function(err, res) {
     }
 })
 
-const PORT = process.env.DB_PORT || 3000; // зачем 3000?
+const PORT = process.env.PORT; // зачем 3000?
 app.listen(PORT, function() {
     console.log('function running on port ', PORT);
+})
+
+app.post('/user', async function(req, res) {
+    try {
+        console.log(req.body);
+        const count_result = await pool.query('SELECT COUNT(*) FROM problems');
+        const count = parseInt(count_result.rows[0].count);
+        const problem_id = count + 1;
+        const {firstname, secondname, lastname} = req.body;
+        const author = firstname + ' ' + secondname + ' ' + lastname;
+        const result = await pool.query(
+            `INSERT INTO problems (id, title, description, author, note, status)
+            VALUES ($1, $2, $3, $4, $5, false) RETURNING *`,
+            [
+                problem_id, 
+                req.body.problemtitle, 
+                req.body.problemdescription, 
+                author, 
+                ""
+            ]
+        );
+
+        res.status(200);
+        res.redirect('/user');
+    } catch (error) {
+        res.status(500).json('Не удалось создать проблему: ' + error.message); // что за фигурные скобки
+        console.log("error: ", error.message);
+    }
+})
+
+app.get('/user', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'user.html'))
 })
